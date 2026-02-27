df63fa23a036fd149d5aaa8270f147a955b0acae
muhammad-danish-webdev <muhammaddanish.webdev@gmail.com>
2026-02-28 02:55:34 +0500

    Code Updated

---
+++ assets/js/partner.js
@@ -0,0 +1,14 @@
+// Partner Section JS Logic
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 0,
+  slidesPerView: "auto", // JS will now stop calculating width
+  speed: 3000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  allowTouchMove: false,
+  observer: true,
+  observeParents: true,
+});
---
+++ assets/js/testimonial.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper(".testimnial-cards-main-container", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: false,
+  grabCursor: true,
+  // Ensure the loop works correctly with custom widths
+  loopedSlides: 4,
+});
